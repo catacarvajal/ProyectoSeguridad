370b0b95b8fad97ddae3b582de9b6761089ed71d
lpozo10 <lpozo10@alumnos.utalca.cl>
2016-12-11 16:37:32 -0300

    arreglando conflicto
    # Conflicts:
    #	app/Http/Controllers/BuscarController.php

--- app/Http/Controllers/BuscarController.php
+++ app/Http/Controllers/BuscarController.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
 use App\Pokemon;
-use App\Commentary;
 
 class BuscarController extends Controller
 {
@@ -28,7 +28,10 @@ class BuscarController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
-    {         
+    {   
+        $ip = $request->ip();
+        
+        Log::info('El usuario: '.\Auth::user()->name.'  con ip  '.$ip.'  Entro al módulo listar pokemon' );      
         $noexiste = 1;
         $Pokemones= Pokemon::paginate(15);
         return view('buscaPokemon')->with('Pokemones',$Pokemones)->with('NoExiste',$noexiste);
@@ -36,7 +39,7 @@ class BuscarController extends Controller
 
     public function postPokemon(Request $request)
     {
-
+        $ip = $request->ip();        
         $nombre=$request->input('name');
         $pokemon = Pokemon::where('name','=',$nombre)->first();
         $Pokemones= Pokemon::paginate(15);
@@ -44,19 +47,23 @@ class BuscarController extends Controller
             
         if($pokemon == NULL)
         {
+            Log::info('El usuario: '.\Auth::user()->name.'  con ip  '.$ip.'  intento buscar un pokemon  '.$nombre.'  , en el formulario buscar' );
              return view('buscaPokemon')->with('Pokemones',$Pokemones)->with('NoExiste',$noexiste);
+             
         }
+        Log::info('El usuario: '.\Auth::user()->name.'  con ip  '.$ip.'  Busco al pokemon  '.$nombre.', en el formulario buscar' );
         return view('indexPokemon')->with('pokemon',$pokemon);
 
     }
 
     public function show( $id )
     {
+        
+  
         $ic = Pokemon::find($id);
-
-        $comentarios = Commentary::where('id_pokemon', $id)->paginate(25);
-
-        return view('show')->with('pokemon',$ic)->with('comentarios', $comentarios);
+        Log::info('El usuario: '.\Auth::user()->name.'con ip:  '.\Request::ip().'  Ingreso a la información del pokemon '.$ic->name );
+        
+        return view('show')->with('pokemon',$ic);
     }
 
 }
